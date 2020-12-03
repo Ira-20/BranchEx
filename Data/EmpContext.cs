a83f2fc4f0dc6ff2d58de798211b2158eaf1ec64
Ira-20 <gupta.ira12@gmail.com>
2020-12-03 16:21:16 +0530

    Third Commit

---
+++ Data/EmpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GitBranchEx.Models;
+
+namespace GitBranchEx.Data
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext (DbContextOptions<EmpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GitBranchEx.Models.Emp> Emp { get; set; }
+    }
+}
---
+++ Migrations/20201203104850_mig1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GitBranchEx.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EDesig = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EDOJ = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
